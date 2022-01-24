0f18cde118d1eb3a1d6fa0d80c58a778daaee3e0
manhng <manhng@sunshinegroup.vn>
2022-01-24 11:41:57 +0700

    sua loi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,211 +1,132 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpParams } from '@angular/common/http';
-import {SelectItem } from 'primeng/api';
-import * as moment from 'moment';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiHrmService } from 'src/app/services/api-hrm/apihrm.service';
+// import { ChartsModule } from 'ng2-charts';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
+
 export class HomeComponent implements OnInit {
-  moment = moment().utcOffset(0, true);
-  constructor(
-    // private apiSmart: ApiSmartService,
-  ) { }
-  revenueTabMenuItems: any = [];
-  bestSellingTabMenuItems: any = [];
-  timelines = [];
-  events: any[];
+  
+  bodyClasses = 'skin-blue sidebar-mini';
+  body: HTMLBodyElement = document.getElementsByTagName('body')[0];
 
-  store_id = '97c54780-187f-4b99-b2c0-380a0f391366';
-  bestSellingTye: number = 1;
-  summaryDataModel = {
-    sales: { total: "", total_amount: "" },
-    returns: { total: "", total_amount: "" }
-  }
-  bestSellingSelectedDate: SelectItem;
-  revenueSelectedDate: SelectItem;
-  revenueTye: number = 1;
-  revenueChart: any = {
-    data: {
-      labels: ["0.1", "0.2"],
-      datasets: [
-        {
-            label: 'My First dataset',
-            backgroundColor: '#42A5F5',
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: 'My Second dataset',
-            backgroundColor: '#FFA726',
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
+  constructor(private HomeService: ApiHrmService, private route: ActivatedRoute, private router: Router) { }
+  columnDefs = [
+    {
+     headerName: 'Thời gian',
+     cellClass: ['border-right'],
+     field: 'date'
     },
-    options: []
-  };
-  bestSellingChart: any = {
-    data: {
-      labels: ["0.1", "0.2"],
-      datasets: [
-        {
-            label: 'My First dataset',
-            backgroundColor: '#42A5F5',
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: 'My Second dataset',
-            backgroundColor: '#FFA726',
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
+    {
+     headerName: 'Hoạt động',
+     cellClass: ['border-right'],
+     field: 'hoat_dong'
+    }];
+    
+  columnDefs1 = [
+    {
+     headerName: 'Tên khách hàng',
+     cellClass: ['border-right'],
+     field: 'tenKH'
     },
-    options: []
-  };
-  colors = [
-    { backgroundColor: "#0063FD", borderColor: "#F3F9FF", hoverBackgroundColor: "#0063FDcc", hoverBorderColor: "#F3F9FF" },
-    { backgroundColor: "#D949D8", borderColor: "#F3F9FF", hoverBackgroundColor: "#D949D8cc", hoverBorderColor: "#F3F9FF" },
-    { backgroundColor: "#FF4F9F", borderColor: "#F3F9FF", hoverBackgroundColor: "#FF4F9Fcc", hoverBorderColor: "#F3F9FF" },
-    { backgroundColor: "#33a8c7", borderColor: "#F3F9FF", hoverBackgroundColor: "#33a8c7cc", hoverBorderColor: "#F3F9FF" },
-    { backgroundColor: "#2ec4b6", borderColor: "#F3F9FF", hoverBackgroundColor: "#2ec4b6cc", hoverBorderColor: "#F3F9FF" },
-  ];
-
-
-  horizontalOptions: any;
-  filterDate: SelectItem[];
-
-  ngOnInit(): void {
-    this.events = [
-      {
-        action: "nhập hàng trả lại",
-        bgClass: "bg-secondary",
-        code: "IT01-TL-2021-000005",
-        employeeName: "Phạm Hà",
-        icon: "fa fa-reply",
-        time: "3 ngày, 2 giờ trước",
-        value: 14000,
-      },
-      {
-        action: "nhập hàng trả lại",
-        bgClass: "bg-secondary",
-        code: "IT01-TL-2021-000005",
-        employeeName: "Phạm Hà",
-        icon: "fa fa-reply",
-        time: "3 ngày, 2 giờ trước",
-        value: 14000,
-      },
-      {
-        action: "nhập hàng trả lại",
-        bgClass: "bg-secondary",
-        code: "IT01-TL-2021-000005",
-        employeeName: "Phạm Hà",
-        icon: "fa fa-reply",
-        time: "3 ngày, 2 giờ trước",
-        value: 14000,
-      },
-      {
-        action: "nhập hàng trả lại",
-        bgClass: "bg-secondary",
-        code: "IT01-TL-2021-000005",
-        employeeName: "Phạm Hà",
-        icon: "fa fa-reply",
-        time: "3 ngày, 2 giờ trước",
-        value: 14000,
-      }
-  ];
-    this.revenueTabMenuItems = [
-      { 
-        label: 'Theo ngày',
-        command: (e) => {
-          this.revenueTye = 1;
-          this.getRevenueChartData();
-        }
-      },
-      { 
-        label: 'Theo thứ',
-        command: (e) => {
-          this.revenueTye = 2;
-          this.getRevenueChartData();
-        }
-      },
-      { 
-        label: 'Theo giờ',
-        command: (e) => {
-          this.revenueTye = 3;
-          this.getRevenueChartData();
-        }
-       }
-    ];
-    this.bestSellingTabMenuItems = [
-      { label: 'Theo doanh thu', icon: '', command: (e) => { this.bestSellingTye = 1; this.getBestSellingProductChartData() } },
-      { label: 'Theo số lượng', icon: '', command: (e) => { this.bestSellingTye = 2; this.getBestSellingProductChartData() } },
-    ];
-
-    this.filterDate = [
-      { label: 'Hôm nay', value: { from: this.moment.clone().startOf("day").format(), to: this.moment.clone().utcOffset(0,true).format() } },
-      { label: 'Hôm qua', value: { from: this.moment.clone().subtract(-1, "days").startOf("day").format(), to: this.moment.clone().subtract(1, "days").endOf("day").format() } },
-      { label: '7 ngày qua', value: { from: this.moment.clone().subtract(7, "days").startOf("day").format(), to: this.moment.clone().format() } },
-      { label: 'Tháng này', value: { from: this.moment.clone().startOf('month').format(), to: this.moment.clone().format() } },
-      { label: 'Tháng trước', value: { from: this.moment.clone().subtract(1, "month").startOf('month').format(), to: this.moment.clone().subtract(1, "month").endOf('month').format() } }
-    ];
-    this.revenueSelectedDate = this.filterDate[0];
-    this.bestSellingSelectedDate = this.filterDate[0];
-    this.GetTimeLineData();
-    this.getRevenueChartData();
-    this.getBestSellingProductChartData();
-    this.getSummaryData();
+    {
+     headerName: 'Nội Dung cần hỗ trợ',
+     cellClass: ['border-right'],
+     field: 'noiDung'
+    }];
+  ngOnInit() {
+    this.drawChart();
+     
   }
-
-  GetTimeLineData() {
-    // const opts = { params: new HttpParams({ fromString: `storeId=${this.store_id}` }) };
-    // this.apiSmart.getTimeLineData(opts.params.toString()).subscribe(rs => {
-    //   if (rs.statusCode) {
-    //     this.timelines = rs.data;
-    //   }
-
-    // });
+  ngOnDestroy() {
   }
+  drawChart(){ 
+    
+ }
+  // line chart
+ chartOptions = {
+  responsive: true
+};
 
-  getDataToday() {
+chartData = [
+  { data: [330, 600, 260, 700], label: 'Nhóm Palace' },
+  { data: [120, 455, 100, 340], label: 'Nhóm Garden' },
+  { data: [45, 67, 800, 500], label: 'Nhóm Penhouse' },
+  { data: [11, 233, 111, 333], label: 'Nhóm Center' },
+  { data: [134, 671, 389, 500], label: 'Nhóm Revierside' }
+];
 
-  }
+chartLabels = ['January', 'February', 'Mars', 'April'];
 
-  getRevenueChartData() {
-    // const opts = { params: new HttpParams({ 
-    // fromString: `storeId=${this.store_id}&type=${this.revenueTye}&from=${this.revenueSelectedDate.value.from}&to=${this.revenueSelectedDate.value.to}` }) };
-    // this.apiSmart.getRevenueChartData(opts.params.toString()).subscribe(rs => {
-    //   if (rs.statusCode) {
-    //     this.revenueChart.data = rs.data
-    //     this.revenueChart.data.datasets.forEach((e, i) => {
-    //       e.backgroundColor = this.colors[i].backgroundColor;
-    //       e.borderColor = this.colors[i].borderColor;
-    //     })
-    //     console.log(this.revenueChart);
-    //   }
-    // });
-  }
+onChartClick(event) {
+  console.log(event);
+}
+// line chart end
+// bar chart
+public barChartOptions:any = {
+  scaleShowVerticalLines: false,
+  responsive: true
+};
+public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+public barChartType:string = 'bar';
+public barChartLegend:boolean = true;
 
-  getBestSellingProductChartData() {
-    // const opts = { params: new HttpParams({ fromString: `storeId=${this.store_id}&type=${this.bestSellingTye}&from=${this.bestSellingSelectedDate.value.from}&to=${this.bestSellingSelectedDate.value.to}` }) };
-    // this.apiSmart.getBestSellingProductChartData(opts.params.toString()).subscribe(rs => {
-    //   if (rs.statusCode) {
-    //     this.bestSellingChart.data = rs.data;
-    //     this.bestSellingChart.data.datasets.forEach((e, i) => {
-    //       e.backgroundColor = this.colors[i].backgroundColor;
-    //       e.borderColor = this.colors[i].borderColor;
-    //     })
-    //   }
-    // });
-  }
+public barChartData:any[] = [
+  //{data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+  {data: [300, 356, 367, 671, 578, 456, 800], label: 'Số lượng khách hàng'}
+];
 
-  getSummaryData() {
-    // console.log('fjodsijfd');
-    // const opts = { params: new HttpParams({ fromString: `storeId=${this.store_id}` }) };
-    // this.apiSmart.getSummaryDashboard(opts.params.toString()).subscribe(rs => {
-    //   if (rs.statusCode) {
-    //     this.summaryDataModel = rs.data
-    //   }
-    // });
-  }
+// events
+public chartClicked(e:any):void {
+  console.log(e);
+}
+
+public chartHovered(e:any):void {
+  console.log(e);
+}
+
+dataLamViecTrongNgay = [
+  {date: '09:15', hoat_dong: 'Gửi mail cho khách'},
+  {date: '11:15', hoat_dong: 'Gửi SMS cho khách'},
+  {date: '13:15', hoat_dong: 'Họp báo cáo thông tin'},
+  {date: '14:15', hoat_dong: 'Tạo khách hàng'},
+  {date: '16:15', hoat_dong: 'Xuống dự án hỗ trợ khách hàng'},
+]
+
+dataHoTroKhachHang = [
+  {tenKH: 'Trần Thị Thùy Dương', noiDung: 'Gửi bộ phận htro, hiện nhà tôi bình nóng lạnh không biết vì lý do gì mà bật lên ko được, không có đèn báo sáng và không có nước nóng tại phòng vệ sinh chung. Vậy nhờ bộ phận kỹ thuật lên sửa chữa giúp'},
+  {tenKH: 'Đỗ Thị Thanh Nga', noiDung: 'Sửa sàn gỗ bị ọp ẹp.xử lý khe trc cửa nhà vs, sơn lại tường 1 số vị trí.'},
+  {tenKH: 'Nguyễn Thị Thanh Hòa', noiDung: '1 số bóng điện không hoạt động Ổ công tắc phòng ngủ nhỏ k khít, sử dụng hơi bị kẹt Đề nghị bql cho người qua kiểm tra và thay thế lúc 13:30 Many thanks.'},
+  {tenKH: 'Nguyễn Minh Quang', noiDung: 'Căn 412 đã quá hơn 100 ngày chưa bàn giao. Hiên trạng toàn đồ cũ. Không đúng như hợp đồng. Tôi xin huỷ hợp đồng. Nếu không tôi nhờ truyền thông căn thiệp.'},
+  {tenKH: 'Lê Mạnh Cường', noiDung: 'Cửa ban công rất khó kéo. Kính đề nghị bql quan tâm sửa sớm.'},
+]
+
+
+public randomize():void {
+  // Only Change 3 values
+  let data = [
+    Math.round(Math.random() * 100),
+    59,
+    80,
+    (Math.random() * 100),
+    56,
+    (Math.random() * 100),
+    40];
+  let clone = JSON.parse(JSON.stringify(this.barChartData));
+  clone[0].data = data;
+  this.barChartData = clone;
+  /**
+   * (My guess), for Angular to recognize the change in the dataset
+   * it has to change the dataset variable directly,
+   * so one way around it, is to clone the data, change it and then
+   * assign it;
+   */
+}
+// bar chart end
+
+load() {}
 
 }
