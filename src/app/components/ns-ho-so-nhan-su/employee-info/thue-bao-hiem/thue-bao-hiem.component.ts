138817789f91cd88b4654e67a64e463a6a028d85
Nguyễn Gia Mạnh <manhng@sunshinegroup.vn>
2022-11-28 15:03:17 +0700

    sua loi

--- src/app/components/ns-ho-so-nhan-su/employee-info/thue-bao-hiem/thue-bao-hiem.component.ts
+++ src/app/components/ns-ho-so-nhan-su/employee-info/thue-bao-hiem/thue-bao-hiem.component.ts
@@ -29,6 +29,7 @@ export class ThueBaoHiemComponent implements OnInit {
 
   ngOnInit(): void {
     this.getEmployeeInfo();
+    this.getEmpDependentPage();
 
   }
   optionsButtonsView = [
@@ -41,7 +42,6 @@ export class ThueBaoHiemComponent implements OnInit {
   getEmployeeInfo(): void {
     this.spinner.show();
     this.listViews = [];
-    this.listViewsForm = [];
     this.detailInfo = null;
     const queryParams = queryString.stringify({ empId: this.empId });
     this.apiService.getEmployeeData('GetEmployeeByInsurance', queryParams).subscribe(results => {
@@ -50,7 +50,6 @@ export class ThueBaoHiemComponent implements OnInit {
           this.codeStaff = getFieldValueAggrid(results.data, 'code');
         }
         this.listViews = cloneDeep(results.data.group_fields || []);
-        this.listViewsForm = cloneDeep(results.data.group_fields || []);
         this.detailInfo = results.data;
         this.spinner.hide();
       }
@@ -67,7 +66,6 @@ export class ThueBaoHiemComponent implements OnInit {
     this.record = event.rowData;
   }
 
- 
   setEmployeeInfo(data) {
     const  params = {
       ...this.detailInfo, group_fields: data
@@ -92,7 +90,148 @@ export class ThueBaoHiemComponent implements OnInit {
 
     }
   }
+  columnDefs = [];
+  gridKey = '';
+  listsData = [];
+  getEmpDependentPage() {
+    this.spinner.show();
+    this.columnDefs = [];
+    const queryParams = queryString.stringify({ empId: this.empId, offSet: 0, pageSize: 10000 });
+    this.apiService.getEmpDependentPage(queryParams).subscribe(repo => {
+      if (repo.status === 'success') {
+        if (repo.data.dataList.gridKey) {
+          this.gridKey = repo.data.dataList.gridKey;
+        }    this.columnDefs[0] = [
+          ...AgGridFn(repo.data.gridflexs || []),
+          {
+            headerName: '',
+            field: 'gridflexdetails1',
+            cellClass: ['border-right', 'no-auto'],
+            pinned: 'right',
+            width: 70,
+            cellRenderer: 'buttonAgGridComponent',
+            cellRendererParams: params => {
+              return {
+                buttons: [
+                  {
+                    onClick: this.ViewForm.bind(this),
+                    label: 'Xem chi tiêt',
+                    icon: 'fa fa-edit edittingg',
+                    key: 'xemchitietlienhe',
+                    class: 'btn-primary mr-1',
+                  },
+                  {
+                    onClick: this.DeleteDependent.bind(this),
+                    label: 'Xóa',
+                    icon: 'pi pi-trash',
+                    key: 'xoa-nguoi-phu-thuoc',
+                    class: 'btn-danger',
+                  },
+                ]
+              };
+            },
+          }];
+          this.listsData = repo.data.dataList.data || [];
 
+      } else {
+        this.spinner.hide();
+      }
+    })
+  }
+  detailFormInfo = null;
+  vitri = 0;
+  titleEmpTrain = '';
+  idFrom = null;
+  displayFormEditDetail = false;
+  ViewForm(event) {
+    this.titleEmpTrain = 'Chỉnh sửa thông tin người phụ thuộc';
+    this.vitri = 0;
+    this.idFrom = event.rowData.dependentId;
+    this.getEmpDependent()
+  }
+
+
+  DeleteDependent(event) {
+    this.confirmationService.confirm({
+      message: 'Bạn có chắc chắn muốn thực xóa người phụ thuộc này?',
+      accept: () => {
+        const queryParams = queryString.stringify({ dependentId: event.rowData.dependentId });
+        this.apiService.delEmpDependent(queryParams).subscribe((results: any) => {
+          if (results.status === 'success') {
+            this.messageService.add({ severity: 'success', summary: 'Thông báo', detail: results.data ? results.data : 'Xóa người phụ thuộc thành công' });
+            this.getEmpDependentPage();
+          } else {
+            this.messageService.add({ severity: 'error', summary: 'Thông báo', detail: results ? results.message : null });
+          }
+        });
+      }
+    });
+  }
+  optionsButtonsPopup = [
+    { label: 'Bỏ qua', value: 'Cancel', class: 'p-button-secondary', icon: 'pi pi-times' },
+    { label: 'Xác nhận', value: 'Update', class: 'btn-accept' }
+  ]
+  gridKeyForm = {
+    index: 0,
+    gridKey: ''
+  }
+  displaySetting = false;
+  CauHinh(type) {
+    this.gridKeyForm = {
+      index: type,
+      gridKey: type === 0 ? this.gridKey : ''
+    }
+    this.displaySetting = true;
+  }
+
+  reloadGetEmpTrainPage() {
+    if(this.gridKeyForm.index === 0) {
+      this.getEmpDependentPage();
+    }
+  }
+
+  addEmpDependent() {
+    this.titleEmpTrain = 'Thêm mới người phụ thuộc';
+    this.vitri = 0;
+    this.idFrom = null;;
+    this.getEmpDependent()
+  }
+
+  getEmpDependent() {
+    const queryParams = queryString.stringify({ empId: this.detailInfo.empId, dependentId: this.idFrom });
+    this.listViewsForm = [];
+    this.apiService.getEmpDependent(queryParams).subscribe(results => {
+      if (results.status === 'success') {
+        this.listViewsForm = cloneDeep(results.data.group_fields);
+        this.detailFormInfo = results.data;
+        this.displayFormEditDetail = true;
+      }
+    })
+
+  }
+  setFormInfo(event) {
+    const param = {
+      ...this.detailFormInfo, group_fields: event
+    }
+    this.spinner.show();
+    if(this.vitri === 0) {
+      this.apiService.setEmpDependent(param).subscribe(results => {
+        if (results.status === 'success') {
+          this.messageService.add({ severity: 'success', summary: 'Thông báo', detail: results.message ? results.message : 'Thêm mới thành công' });
+          this.displayFormEditDetail = false;
+          this.getEmployeeInfo();
+          this.spinner.hide();
+        } else {
+          this.spinner.hide();
+          this.messageService.add({ severity: 'error', summary: 'Thông báo', detail: results.message });
+        }
+      })
+    }
+  }
+
+  cancelFormInfo(event) {
+
+  }
 
 }
 
