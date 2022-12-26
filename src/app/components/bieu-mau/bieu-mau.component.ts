0a0cd900bc06e9cb812a2d23849fb2be8513c90b
longnc@sunshinegroup.vn <longnc@unicloud.com.vn>
2022-12-26 10:20:41 +0700

    tai lieu ca nhan

--- src/app/components/bieu-mau/bieu-mau.component.ts
+++ src/app/components/bieu-mau/bieu-mau.component.ts
@@ -412,19 +412,19 @@ export class BieuMauComponent implements OnInit, OnDestroy {
         showDisabledCheckboxes: true,
       },
       ...AgGridFn(this.cols.filter((d: any) => !d.isHide)),
-      // {
-      //   headerComponentParams: {
-      //     template:
-      //     `<button  class="btn-button" id="${this.gridKey}"> <span class="pi pi-plus action-grid-add" ></span></button>`,
-      //   },
-      //   filter: '',
-      //   maxWidth: 80,
-      //   pinned: 'right',
-      //   cellRenderer: 'buttonAgGridComponent',
-      //   cellClass: ['border-right', 'no-auto', 'cell-options'],
-      //   cellRendererParams: (params: any) => this.showButtons(params),
-      //   field: 'checkbox'
-      // },
+      {
+        headerComponentParams: {
+          template:
+          `<button  class="btn-button" id="${this.gridKey}"> <span class="pi pi-plus action-grid-add" ></span></button>`,
+        },
+        filter: '',
+        maxWidth: 80,
+        pinned: 'right',
+        cellRenderer: 'buttonAgGridComponent',
+        cellClass: ['border-right', 'no-auto', 'cell-options'],
+        cellRendererParams: (params: any) => this.showButtons(params),
+        field: 'checkbox'
+      },
     ]
   }
 
@@ -599,7 +599,6 @@ export class BieuMauComponent implements OnInit, OnDestroy {
     }
     
   }
-
  
   rowSelected(data) {
     this.listDataSelect = []
