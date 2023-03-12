88f029e0cd30173e6d3ca37be21966fc4450f138
manhng <manhng@sunshine.group.vn>
2023-03-12 07:36:43 +0700

    sua loi

--- src/app/components/cs-cham-cong/cs-cham-cong.component.ts
+++ src/app/components/cs-cham-cong/cs-cham-cong.component.ts
@@ -157,14 +157,10 @@ detailInfoFilter = null;
 
   Export() {
     let params: any = {... this.query};
-    // delete params.organizeId
     delete params.fromDate
     delete params.toDate
-    // params.FromDate = moment(new Date(this.query.fromDate)).format('YYYY-MM-DD')
-    // params.ToDate = moment(new Date(this.query.toDate)).format('YYYY-MM-DD')
-
     const queryParams = queryString.stringify(params);
-    this.spinner.show();
+    // this.spinner.show();
     this.apiService.getExportReport('ExportBangLuongThang',queryParams)
     .pipe(takeUntil(this.unsubscribe$))
     .subscribe(results => {
