ff9e418ae29be07833f64d4d7b5f9250d6389b0b
longnc@sunshinegroup.vn <longnc@unicloud.com.vn>
2023-05-24 15:15:39 +0700

    css

--- src/app/components/cai-dat-to-chuc/lich-lam-viec/lich-lam-viec.component.ts
+++ src/app/components/cai-dat-to-chuc/lich-lam-viec/lich-lam-viec.component.ts
@@ -179,7 +179,7 @@ export class LichLamViecComponent implements OnInit {
       buttons: [
         {
           onClick: this.editRow.bind(this),
-          label: 'Thông tin chi tiết',
+          label: 'Xem',
           icon: 'fa fa-eye',
           class: 'btn-primary mr5',
           hide: CheckHideAction(MENUACTIONROLEAPI.GetWorktimePage.url, ACTIONS.VIEW)
@@ -205,7 +205,7 @@ export class LichLamViecComponent implements OnInit {
             `<button  class="btn-button" id="${this.gridKey}"> <span class="pi pi-plus action-grid-add" ></span></button>`,
         },
         filter: '',
-        width: 70,
+        width: 100,
         pinned: 'right',
         cellRenderer: 'buttonAgGridComponent',
         cellClass: ['border-right', 'no-auto'],
