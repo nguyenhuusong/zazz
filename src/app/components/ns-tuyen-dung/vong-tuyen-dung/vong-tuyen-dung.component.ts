62c27a852f983f2e1d0adc8e5997c8a6313505d4
longnc@sunshinegroup.vn <longnc@unicloud.com.vn>
2023-06-19 18:08:15 +0700

    fix

--- src/app/components/ns-tuyen-dung/vong-tuyen-dung/vong-tuyen-dung.component.ts
+++ src/app/components/ns-tuyen-dung/vong-tuyen-dung/vong-tuyen-dung.component.ts
@@ -177,10 +177,6 @@ import { Router } from '@angular/router';
       this.columnDefs = [
         ...AgGridFn(this.cols.filter((d: any) => !d.isHide)),
         {
-          headerComponentParams: {
-            template:
-            `<button  class="btn-button" id="${this.gridKey}"> <span class="pi pi-plus action-grid-add" ></span></button>`,
-          },
           filter: '',
           width: 100,
           pinned: 'right',
@@ -188,7 +184,7 @@ import { Router } from '@angular/router';
           cellClass: ['border-right', 'no-auto'],
           cellRendererParams: (params: any) => this.showButtons(params),
           checkboxSelection: false,
-          field: 'checkbox'
+          field: ''
         }]
     }
   
